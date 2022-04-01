34d8244fd2cf37f1876ecfce1cef201257a2af8f
Kaan Bilgi <54486031+fquer@users.noreply.github.com>
2022-04-01 23:40:53 +0300

    Add files via upload

---
+++ notificator.py
@@ -0,0 +1,24 @@
+
+import pyautogui
+import winsound
+import os
+from PIL import ImageGrab
+from functools import partial
+ImageGrab.grab = partial(ImageGrab.grab, all_screens=True)
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+        
+os.system('cls')
+print("Ready !\nWaiting for invitations...")
+
+while True:
+    
+    x = pyautogui.locateCenterOnScreen('new_request.PNG', confidence = 0.9)
+    
+    if x != None:
+        try:
+            winsound.PlaySound(dname+'\\sound.wav', winsound.SND_FILENAME)
+        except:
+            print("Audio file not found !")
